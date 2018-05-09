30349eefed2f5558f3e1acc20e706cde27b25953
HelloZola <643074224@qq.com>
2018-05-09 17:36:34 +0800

    代码优化

--- heading-spring/src/main/java/com/cn/myspring/controller/HelloworldController.java
+++ heading-spring/src/main/java/com/cn/myspring/controller/HelloworldController.java
@@ -1,36 +1,31 @@
 package com.cn.myspring.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import example.aspect.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import example.aspect.IUserService;
-import example.aspect.UserServiceImpl;
-
 @Controller
 @RequestMapping("/HelloworldController")
 public class HelloworldController{
-	
+
+	@Autowired
+	IUserService userService;
+
 	/**
 	 * test绫�
 	 * http://localhost:8080/heading-spring/HelloworldController/helloworld.do
-	 * 
 	 * @return
 	 */
 	@RequestMapping("/helloworld")
     public ModelAndView hello() {
-		
 		System.out.println("fsdfsfsfdfdsf****************************");
 		System.out.println("fsdfsfsfdfdsf****************************");
 		System.out.println("fsdfsfsfdfdsf****************************");
 		System.out.println("fsdfsfsfdfdsf****************************");
-		
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/contextLoaderListener.xml");
-		UserServiceImpl userService = applicationContext.getBean(UserServiceImpl.class);
+		System.out.println("test5");
 		userService.addUser();
-		
 		ModelAndView model = new ModelAndView();
 		model.addObject("fsfsdfdsf");
         return model;
