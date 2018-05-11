cc56f490f4053d6f9575709242b46693ec679768
HelloZola <643074224@qq.com>
2018-05-11 15:41:57 +0800

    提交netty简单应答demo

---
+++ heading-netty/src/main/java/com/vi/example1/server/Calculator.java
@@ -0,0 +1,14 @@
+package com.vi.example1.server;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public enum Calculator {
+    Instance;
+    private final static ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
+
+    public Object cal(String expression) throws ScriptException {
+        return jse.eval(expression);
+    }
+}
---
+++ heading-netty/src/main/java/com/vi/example1/server/TimeServer.java
@@ -0,0 +1,48 @@
+package com.vi.example1.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        // 配置服务端的NIO，循环事件线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();//配置类
+            //NioServerSocketChannel作为channel类，它的功能对应于JDK NIO类库中的ServerSocketChannel
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new TimeServerHandler());//绑定事件处理类
+            // 绑定端口，同步等待成功
+            System.out.println("开始启动服务。。。");
+            ChannelFuture f = b.bind(port).sync();
+            System.out.println("启动服务完成。。。");
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+            System.out.println("关闭服务完成。。。");
+        } finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        new TimeServer().bind(port);
+    }
+}
