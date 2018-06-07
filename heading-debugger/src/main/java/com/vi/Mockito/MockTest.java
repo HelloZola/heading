bbdae7884fecc128036dbbb3161820228dcdb25f
Aida <643074224@qq.com>
2018-06-07 23:01:25 +0800

    mockito测试代码提交

--- heading-debugger/src/main/java/com/vi/Mockito/MockTest.java
+++ heading-debugger/src/main/java/com/vi/Mockito/MockTest.java
@@ -13,15 +13,17 @@ public class MockTest {
 
     @Test
     public void test1() {
+        //初始化内容
         PersonDao mockDao = mock(PersonDao.class);
         Person person = new Person();
         person.setAge(12);
         person.setName("chenkangliu");
         //设置模拟调用setPersion方法
         PersonService service = new PersonService(mockDao);
+        //设置模拟返回条件
         when(mockDao.setPerson(person)).thenReturn(new Person("aida", 12));
+        //开始调用
         Person ress = service.testPersionDao(person);
-        System.out.println(ress);
         //验证是否调了一次mockDao的setPerson方法
         verify(mockDao, times(1)).setPerson(isA(Person.class));
         assertEquals(new Person("aida", 12).toString(), ress.toString());
